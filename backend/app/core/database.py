f635410b902ca765be5af1d22d97e83b5aeda6b0
lazypandaa <eswarkrishnak2005@email.com>
2026-01-31 02:52:16 +0530

    Fix DB initialization to avoid startup crash

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,16 +1,18 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
-
-MONGODB_URL = "mongodb+srv://freshmart_user:eASMfdl5SXeGqOfx@hack.8syianl.mongodb.net/FreshMart?retryWrites=true&w=majority&appName=hack"
-DATABASE_NAME = "FreshMart"
+import os
 
 client = None
 database = None
 
 async def connect_to_mongo():
     global client, database
-    client = AsyncIOMotorClient(MONGODB_URL, server_api=ServerApi('1'))
-    database = client[DATABASE_NAME]
+    mongodb_url = os.getenv("MONGODB_URL")
+    if not mongodb_url:
+        raise RuntimeError("MONGODB_URL environment variable is not set")
+    
+    client = AsyncIOMotorClient(mongodb_url, server_api=ServerApi('1'))
+    database = client["FreshMart"]
     print("Connected to MongoDB")
 
 async def close_mongo_connection():
